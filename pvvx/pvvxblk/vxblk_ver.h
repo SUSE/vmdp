e323a9f0bd4b961abba0c5f032fffb135d45cf58
Kirk Allan <kallan@suse.com>
2021-03-08 11:55:46 -0700

    Bump version

--- pvvx/pvvxblk/vxblk_ver.h
+++ pvvx/pvvxblk/vxblk_ver.h
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright 2013-2020 SUSE LLC
+ * Copyright 2013-2021 SUSE LLC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,13 +28,13 @@
 #define _VXBLK_VER_H
 
 #ifdef IS_STORPORT
-#define VER_FILEVERSION             2,6,0,06
-#define VER_FILEVERSION_STR         "2.6.0.06\0"
+#define VER_FILEVERSION             2,6,0,07
+#define VER_FILEVERSION_STR         "2.6.0.07\0"
 #else
-#define VER_FILEVERSION             2,6,0,06
-#define VER_FILEVERSION_STR         "2.6.0.06\0"
+#define VER_FILEVERSION             2,6,0,07
+#define VER_FILEVERSION_STR         "2.6.0.07\0"
 #endif
 
-#define VER_LEGALCOPYRIGHT_STR      "Copyright \251 2013-2020 SUSE All rights reserved.", "\0"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright \251 2013-2021 SUSE All rights reserved.", "\0"
 
 #endif
--- pvvx/pvvxscsi/vxscsi_ver.h
+++ pvvx/pvvxscsi/vxscsi_ver.h
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright 2011-2012 Novell, Inc.
- * Copyright 2012-2020 SUSE LLC
+ * Copyright 2012-2021 SUSE LLC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,13 +29,13 @@
 #define _VXSCSI_VER_H
 
 #ifdef IS_STORPORT
-#define VER_FILEVERSION             1,0,1,05
-#define VER_FILEVERSION_STR         "1.0.1.05\0"
+#define VER_FILEVERSION             1,0,1,06
+#define VER_FILEVERSION_STR         "1.0.1.06\0"
 #else
-#define VER_FILEVERSION             1,0,1,05
-#define VER_FILEVERSION_STR         "1.0.1.05\0"
+#define VER_FILEVERSION             1,0,1,06
+#define VER_FILEVERSION_STR         "1.0.1.06\0"
 #endif
 
-#define VER_LEGALCOPYRIGHT_STR      "Copyright \251 2013-2020 SUSE All rights reserved.", "\0"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright \251 2013-2021 SUSE All rights reserved.", "\0"
 
 #endif
